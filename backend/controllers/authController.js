ab3adc713850d0641ba0a067ecc892aac2b08c73
Ezazulmahi <mahiezazulhaque@gmail.com>
2026-06-15 17:40:32 +0600

    fix: allow cross-site session cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,8 @@ const User = require('../models/user')
 const COOKIE_NAME = 'scholarship_session'
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000,
 }
 
